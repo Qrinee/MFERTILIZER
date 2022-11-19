f537b06ffded3592abe957d125509c4f17da00f9
Qrinee <61843273+Qrinee@users.noreply.github.com>
2022-11-19 19:39:50 +0100

    Dodaj pliki projektów.

---
+++ WpfApp1/ItemCompost.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logika interakcji dla klasy ItemCompost.xaml
+    /// </summary>
+    public partial class ItemCompost : UserControl
+    {
+        public ItemCompost()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static bool added = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            addedCompost();
+        }
+
+
+        
+        public void addedCompost()
+        {
+            form1.Visibility = Visibility.Hidden;  
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            oneItem.Visibility = Visibility.Hidden;
+            form1.Visibility = Visibility.Visible;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            form1.Visibility = Visibility.Hidden;
+            forum.Visibility = Visibility.Hidden;
+            oneItem.Visibility = Visibility.Visible;
+            buttonAdd.Visibility = Visibility.Visible;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            forum.Visibility = Visibility.Visible;
+            oneItem.Visibility = Visibility.Hidden;
+            buttonAdd.Visibility = Visibility.Hidden;
+        }
+
+     
+    }
+}
